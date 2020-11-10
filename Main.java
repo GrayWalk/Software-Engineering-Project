eb4660946decc5ed47fa94376df69856b9ff2703
Taylor Young <taylornealyoung@gmail.com>
2020-11-10 16:22:14 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
-	public static void main{
+	public static void main(String[] args){
 		System.out.println("Grayson's a sexy mofo");
 		System.out.println("Taylor's a sexy mofo");
 	}
