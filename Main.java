a26606e0a1180a39921d37b9409a0f2a41a976b2
GrayWalk <72168177+GrayWalk@users.noreply.github.com>
2020-11-10 16:38:54 -0700

    boi

--- Main.java
+++ Main.java
@@ -3,5 +3,12 @@ public class Main{
 		System.out.println("Grayson's a sexy mofo");
 		System.out.println("Taylor's a sexy mofo");
 		System.out.println("Not looking forward to coding a GUI");
+
+
+
+
+
+
+		
 	}
 }
